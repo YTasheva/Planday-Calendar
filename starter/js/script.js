71795aace897229da2ad2a262189f1949128fe1c
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-05 22:12:32 +0000

    Refactor timeblock color logic and add save button functionality

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,69 +1,47 @@
 //Current day and time
 var dayNtime = $("#currentDay").text(dayjs().format('MMM D dddd, h:mmA'));
 
+//Timeblocks color
 
-$(function() {
-    $(".saveBtn").on("click", function () {
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, value);
-    })
+    $(".time-block").each(function () {
+        var currentHour = parseInt(dayjs().format('H'));
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-    var pop = document.getElementById("pop-up");
-    var btn = document.getElementById("btn");
-    var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function () {
-        pop.style.display = "block";
-    }
-    span.onclick = function () {
-        pop.style.display = "none";
-    }
-    window.onclick = function (event) {
-        if (event.target == pop) {
-            pop.style.display = "none";
+        if (blockHour < currentHour) {
+            // past white color block
+            $(this).addClass("past");
         }
-    }
-
-    $(function() {
-        $(".deleteBtn").on("click", function () {
-            var value = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("id");
-            localStorage.removeItem(time, value);
-            $(this).siblings(".description").val("");
-        });
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            // present red color block
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            // future blue color block
+            $(this).addClass("future");
+        }
     });
 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    $("#currentTime");
-    hourUpdater();
-    setInterval(hourUpdater, 15000);
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
-});
 
-var pop = document.getElementById("pop-up");
-var btn = document.getElementById("btn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function () {
-pop.style.display = "block";
-}
-span.onclick = function () {
-pop.style.display = "none";
-}
-window.onclick = function (event) {
-if (event.target == pop) {
-    pop.style.display = "none";
-}
-}
+//saveBtn click event
+var saveBtn=$(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        if (value) {
+            alert("The text is saved!");
+        }
+        else {
+            alert("The text is not saved!");
+        }
+        localStorage.setItem(time, value);
+    });
+
+    //load any saved data from LocalStorage
 
 
 
-
+    
+    loadTimeBlocks();
