8bc59a49d48d1dd6bbc5c74625c03040dcfa83ad
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-06 15:05:48 +0000

    Fix timeblocks color and saveBtn click event

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,55 +1,45 @@
 //Current day and time
 var saveBtn = $(".saveBtn");
-$("#currentDay").text(dayjs().format('MMM D dddd, h:mmA'));
-
+$("#currentDay").text(dayjs().format("MMM D dddd, h:mmA"));
 
 //Timeblocks color
-function blockColor() {
-    var hour = dayjs().hours();
+$(".time-block").each(function () {
+  var currentHour = parseInt(dayjs().format("H"));
+  var colorBlock = parseInt($(this).attr("id"));
 
-    $(".time-block").each(function () {
-        var currentHour = parseInt($(this).attr("id"));
-
-        if (currentHour > hour) {
-            $(this).addClass("future");
-        }
-        else if (blockHour === currentHour) {
-            $(this).addClass("present");
-        }
-        else {
-            $(this).addClass("past");
-          
-        }
-    })
-};
-
+  if (colorBlock > currentHour) {
+    $(this).addClass("future");
+  } else if (colorBlock < currentHour) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("present");
+  }
+});
 
 //saveBtn click event
-var saveBtn=$(".saveBtn").on("click", function () {
-        var description = $(this).siblings(".description").val();
-        var time = $(this).siblings(".hour").text();
+var saveBtn = $(".saveBtn").on("click", function () {
+  var description = $(this).siblings(".description").val();
+  var time = $(this).siblings(".hour").text();
 
-        if (description) {
-            alert("The text is saved!");
-        }
-        else {
-            alert("The text is not saved!");
-        }
-        localStorage.setItem(time, description);
-    });
+  if (description) {
+    alert("The text is saved!");
+  } else {
+    alert("The text is not saved!");
+  }
+  localStorage.setItem(time, description);
+});
 
 //load any saved data from LocalStorage
 
 function loadTimeBlocks() {
-    $(".hour").each(function () {
-        var currentHour = $(this).text();
-        var schedule = localStorage.getItem(currentHour);
+  $(".hour").each(function () {
+    var currentHour = $(this).text();
+    var schedule = localStorage.getItem(currentHour);
 
-        if (schedule !== null) {
-            $(this).children(".description").val(schedule);
-        
-        }
-    });
+    if (schedule !== null) {
+      $(this).children(".description").val(schedule);
+    }
+  });
 }
 
 blockColor();
